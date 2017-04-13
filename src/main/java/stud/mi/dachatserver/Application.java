722818698d4888bd6e9fc5e061e1c932034cbddd
Jan-Niklas Keck <jk165@hdm-stuttgart.de>
2017-04-13 16:50:35 +0200

    enable serversocket

--- src/main/java/stud/mi/dachatserver/Application.java
+++ src/main/java/stud/mi/dachatserver/Application.java
@@ -3,14 +3,21 @@ package stud.mi.dachatserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import stud.mi.dachatserver.chatserver.Server;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		// new Thread(() -> {
-		// Server s = new Server(1234);
-		// s.start();
-		// }).start();
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				Server s = new Server(1234);
+				s.start();
+			}
+
+		}).start();
 		SpringApplication.run(Application.class, args);
 	}
 
