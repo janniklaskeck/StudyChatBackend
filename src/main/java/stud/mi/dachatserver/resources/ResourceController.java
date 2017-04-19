5888d399d1a965a6954d1b15a739903ca506bcfa
Jan-Niklas Keck <jk165@hdm-stuttgart.de>
2017-04-19 13:55:56 +0200

    remove socket stuff

--- src/main/java/stud/mi/dachatserver/resources/ResourceController.java
+++ src/main/java/stud/mi/dachatserver/resources/ResourceController.java
@@ -1,29 +1,33 @@
 package stud.mi.dachatserver.resources;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import stud.mi.dachatserver.chatserver.ServerManager;
-import stud.mi.dachatserver.dto.Session;
 
 @RestController
 @RequestMapping("/")
 public class ResourceController {
 
-	@GetMapping("/session")
-	public Session createSession() {
-		return ServerManager.getInstance().createServer(1234);
+	private static String tempMessages = "";
+
+	@PostMapping("/message")
+	@ResponseStatus(HttpStatus.OK)
+	public String addMessage(@RequestBody String input) {
+		addMessageToDB(input);
+		return input;
 	}
 
-	@PostMapping("/session")
-	public Session freeSession() {
-		return ServerManager.getInstance().stopServer(1234);
+	private static void addMessageToDB(final String message) {
+		tempMessages += message + System.lineSeparator();
 	}
 
-	@GetMapping("/")
-	public String documentation() {
-		return "<html><a href='/session'>Click to get Session</a>";
+	@GetMapping("/messages")
+	@ResponseStatus(HttpStatus.OK)
+	public String getMessages() {
+		return tempMessages;
 	}
 }
